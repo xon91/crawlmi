f4bb4e6313be65a89730acc458768cd3cd37f117
Michal Danilak <mimino.coder@gmail.com>
2013-02-11 21:36:48 +0100

    Generalized the interface of the base Queue class.

--- crawlmi/queue/queue.py
+++ crawlmi/queue/queue.py
@@ -4,46 +4,46 @@ class Queue(object):
         self._closed = False
         self._num_objects = 0
 
-    def push(self, value):
+    def push(self, *args, **kwargs):
         if self._closed:
             raise RuntimeError('pushing to already closed queue')
         self._num_objects += 1
-        self._push(value)
+        self._push(*args, **kwargs)
 
-    def peek(self):
+    def peek(self, *args, **kwargs):
         if self._closed:
             raise RuntimeError('peeking already closed queue')
         if self._num_objects == 0:
             raise IndexError('peeking in empty queue')
-        return self._peek()
+        return self._peek(*args, **kwargs)
 
-    def pop(self):
+    def pop(self, *args, **kwargs):
         if self._closed:
             raise RuntimeError('popping from already closed queue')
         if self._num_objects == 0:
             raise IndexError('popping from empty qeueue')
         self._num_objects -= 1
-        return self._pop()
+        return self._pop(*args, **kwargs)
 
-    def close(self):
+    def close(self, *args, **kwargs):
         if self._closed:
             raise RuntimeError('closing already closed queue')
         self._closed = True
-        self._close()
+        self._close(*args, **kwargs)
 
     def __len__(self):
         if self._closed:
             raise RuntimeError('`len` on already closed queue')
         return self._num_objects
 
-    def _push(self):
+    def _push(self, *args, **kwargs):
         raise NotImplementedError()
 
-    def _peek(self):
+    def _peek(self, *args, **kwargs):
         raise NotImplementedError()
 
-    def _pop(self):
+    def _pop(self, *args, **kwargs):
         raise NotImplementedError()
 
-    def _close(self):
+    def _close(self, *args, **kwargs):
         raise NotImplementedError()
