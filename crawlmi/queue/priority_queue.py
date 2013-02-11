5384e7f0d7103d2a29d6645bf5d89294a67d69b6
Michal Danilak <mimino.coder@gmail.com>
2013-02-11 21:51:26 +0100

    Added priority_queue.

---
+++ crawlmi/queue/priority_queue.py
@@ -0,0 +1,55 @@
+from .heap import Heap
+from .queue import Queue
+
+
+class PriorityQueue(Queue):
+    '''Implementation of the priority queue with the following properties:
+        - objects with the higher `priority` are popped first
+        - objects with the same `priority` are popped in the FIFO order
+
+    Implementation combines the asymptotic speed of the heap with the practical
+    speed of the ordinary queue. In other words, it works pretty fast, when
+    there are many different priorities and even faster, when there are only
+    few different priorities.
+
+    PriorityQueue internally uses the implementation of the ordinary queue,
+    defined through `qfactory`. Internal queue has to inherit from `Queue`.
+
+    The constructor receives a `qfactory` argument, which is a callable used to
+    instantiate a new (internal) queue when a new priority is allocated. The
+    `qfactory` function is called with the priority number as first and only
+    argument.
+    '''
+
+    def __init__(self, qfactory):
+        super(PriorityQueue, self).__init__()
+        self.qfactory = qfactory
+        self._heap = Heap()
+        self._active_queues = {}
+
+    def _push(self, priority, value):
+        if priority not in self._active_queues:
+            self._active_queues[priority] = self.qfactory(priority)
+            self._heap.push(priority)
+        self._active_queues[priority].push(value)
+
+    def _peek(self):
+        top_priority = self._heap.peek()
+        return self._active_queues[top_priority].peek()
+
+    def _pop(self):
+        top_priority = self._heap.peek()
+        q = self._active_queues[top_priority]
+        result = q.pop()
+        if len(q) == 0:
+            del self._active_queues[top_priority]
+            q.close()
+            self._heap.pop()
+        return result
+
+    def _close(self):
+        for p, q in self._active_queues.iteritems():
+            q.close()
+        del self._active_queues
+        self._heap.close()
+        del self._heap
---
+++ crawlmi/tests/test_priority_queue.py
@@ -0,0 +1,77 @@
+import random
+import unittest2
+
+from crawlmi.queue.memory_queue import MemoryQueue
+from crawlmi.queue.priority_queue import PriorityQueue
+
+
+def qfactory(priority):
+    return MemoryQueue()
+
+
+class PriorityQueueTest(unittest2.TestCase):
+
+    def test_basic(self):
+        q = PriorityQueue(qfactory)
+        self.assertEqual(len(q), 0)
+        self.assertRaises(IndexError, q.peek)
+        self.assertRaises(IndexError, q.pop)
+        q.push(42, 42)
+        self.assertEqual(len(q), 1)
+        self.assertEqual(q.peek(), 42)
+        q.push(47, 47)
+        self.assertEqual(len(q), 2)
+        self.assertEqual(q.peek(), 47)
+
+        self.assertEqual(q.pop(), 47)
+        self.assertEqual(q.peek(), 42)
+        self.assertEqual(q.pop(), 42)
+        self.assertRaises(IndexError, q.pop)
+        self.assertEqual(len(q), 0)
+
+    def test_close(self):
+        q = PriorityQueue(qfactory)
+        q.push(1, 1)
+        self.assertEqual(len(q), 1)
+        q.close()
+        self.assertRaises(RuntimeError, q.push, 10)
+        self.assertRaises(RuntimeError, q.pop)
+        self.assertRaises(RuntimeError, q.peek)
+        self.assertRaises(RuntimeError, q.close)
+        self.assertRaises(RuntimeError, len, q)
+
+    def _pop_all(self, q):
+        steps = len(q)
+        return [q.pop() for i in xrange(steps)]
+
+    def test_duplicate(self):
+        q = PriorityQueue(qfactory)
+        q.push(1, 'a')
+        q.push(1, 'b')
+        self.assertEqual(len(q), 2)
+        q.push(2, 'c')
+        q.push(3, 'd')
+        q.push(2, 'e')
+        q.push(3, 'f')
+        self.assertEqual(len(q), 6)
+        self.assertListEqual(self._pop_all(q), ['d', 'f', 'c', 'e', 'a', 'b'])
+
+    def test_random_case(self):
+        rnd = random.Random(47)
+        q = PriorityQueue(qfactory)
+        values = []
+        operations = []
+        for step in xrange(100):
+            if len(q) and rnd.randint(1, 3) == 1:
+                operations.append('pop')
+                self.assertEqual(values.pop()[1], q.pop(), operations)
+                if values:
+                    self.assertEqual(values[-1][1], q.peek(), operations)
+            else:
+                v = rnd.randint(1, 50)
+                q.push(v, -step)
+                values.append((v, -step))
+                operations.append('add %s' % v)
+                values.sort()
+                self.assertEqual(values[-1][1], q.peek(), operations)
+            self.assertEqual(len(q), len(values), operations)
