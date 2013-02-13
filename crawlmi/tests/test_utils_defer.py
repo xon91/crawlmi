4d83b5d84fc77ec6c420040e934e273eaa19799e
Michal Danilak <mimino.coder@gmail.com>
2013-02-13 02:09:17 +0100

    Created ScheduledCall class.

---
+++ crawlmi/tests/test_utils_defer.py
@@ -0,0 +1,108 @@
+import unittest2
+
+from twisted.internet import task
+
+from crawlmi.utils.defer import ScheduledCall
+
+
+class ModifiedObject(object):
+    def __init__(self):
+        self.args = None
+        self.kwargs = None
+
+    def func(self, *args, **kwargs):
+        self.args = args
+        self.kwargs = kwargs
+
+
+class ScheduledCallTest(unittest2.TestCase):
+    default_args = (10, 'hello')
+    default_kwargs = {'a': 47, 'b': 'c'}
+
+    def setUp(self):
+        self.clock = task.Clock()
+        self.obj = ModifiedObject()
+        self.sc = ScheduledCall(self.obj.func, *self.default_args,
+                                **self.default_kwargs)
+        self.sc._clock = self.clock
+
+    def _check(self, args, kwargs):
+        if args is None:
+            self.assertIsNone(self.obj.args)
+        else:
+            self.assertTupleEqual(self.obj.args, args)
+
+        if kwargs is None:
+            self.assertIsNone(self.obj.kwargs)
+        else:
+            self.assertEqual(self.obj.kwargs, kwargs)
+
+    def test_get_time_and_is_scheduled(self):
+        self.clock.advance(10)
+
+        self.assertFalse(self.sc.is_scheduled())
+        self.assertEqual(self.sc.get_time(), 0)
+        self.sc.schedule(5)
+        self.assertTrue(self.sc.is_scheduled())
+        self.assertEqual(self.sc.get_time(), 15)
+        self.clock.advance(5)
+        self.assertFalse(self.sc.is_scheduled())
+        self.assertEqual(self.sc.get_time(), 0)
+
+    def test_no_delay(self):
+        self.sc.schedule()
+        self._check(None, None)
+        self.clock.advance(0)
+        self._check(self.default_args, self.default_kwargs)
+
+    def test_default(self):
+        self.assertTrue(self.sc.schedule(5))
+        self._check(None, None)
+        self.clock.advance(1)
+        self.assertFalse(self.sc.schedule(1))
+        self.clock.advance(2)
+        self._check(None, None)
+        self.clock.advance(3)
+        self._check(self.default_args, self.default_kwargs)
+
+    def test_cancel(self):
+        self.sc.schedule(5)
+        self.clock.advance(3)
+        self.sc.cancel()
+        self.clock.advance(3)
+        self._check(None, None)
+        self.assertTrue(self.sc.schedule(1))
+        self.clock.advance(1)
+        self._check(self.default_args, self.default_kwargs)
+
+    def test_overwrite(self):
+        over_args = ('crawlmi',)
+        over_kwargs = {'a': 50, 'd': 'e'}
+        self.sc.schedule(5, *over_args, **over_kwargs)
+        self.clock.advance(5)
+        self._check(over_args, over_kwargs)
+
+    def test_partial_overwrite(self):
+        over_args = ('crawlmi',)
+        self.sc.schedule(5, *over_args)
+        self.clock.advance(5)
+        self._check(over_args, {})
+
+    def test_nested_call(self):
+        args1 = ('a',)
+        kwargs1 = {'a': 'b'}
+        args2 = ('b',)
+        kwargs2 = {'b': 'c'}
+
+        def func(*args, **kwargs):
+            self.obj.func(*args, **kwargs)
+            self.sc.schedule(4, *args2, **kwargs2)
+        self.sc._func = func
+        self.sc.schedule(3, *args1, **kwargs1)
+        self.clock.advance(3)
+        self.assertIsNotNone(self.sc._call)
+        self._check(args1, kwargs1)
+        self.clock.advance(3)
+        self._check(args1, kwargs1)
+        self.clock.advance(1)
+        self._check(args2, kwargs2)
---
+++ crawlmi/utils/defer.py
@@ -0,0 +1,69 @@
+from twisted.internet import reactor
+
+
+class ScheduledCall(object):
+    '''Schedule the function call for the future.
+    Also ensure, that the function can be scheduled only once at a time.
+    '''
+
+    def __init__(self, func, *default_args, **default_kwargs):
+        '''When scheduling the function, use `default_args` and
+        `default_kwargs`, when no arguments are given to `schedule` function.
+        '''
+
+        self._func = func
+        self._default_args = default_args
+        self._default_kwargs = default_kwargs
+        self._next_args = None
+        self._next_kwargs = None
+        self._call = None
+        # _clock is used in unittests
+        self._clock = None
+
+    def schedule(self, delay=0, *args, **kwargs):
+        '''Schedule the function call.
+        `args` and `kwargs` overwrite `default_args` and `default_kwargs` if
+        ANY of them is given.
+        Return True, if the schedule was successfull.
+        '''
+
+        if self._call is None:
+            if args or kwargs:
+                self._next_args = args
+                self._next_kwargs = kwargs
+            else:
+                self._next_args = self._default_args
+                self._next_kwargs = self._default_kwargs
+
+            if self._clock is None:
+                self._call = reactor.callLater(delay, self)
+            else:
+                self._call = self._clock.callLater(delay, self)
+            return True
+        return False
+
+    def cancel(self):
+        if self._call:
+            self._call.cancel()
+            self._call = None
+            self._next_args = None
+            self._next_kwargs = None
+
+    def is_scheduled(self):
+        return self._call is not None
+
+    def get_time(self):
+        '''Return the time for which is this function scheduled.'''
+        if self._call:
+            return self._call.getTime()
+        else:
+            return 0
+
+    def __call__(self):
+        # order of the following operations is very important in a case
+        # when self._func schedules itself
+        self._call = None
+        args, self._next_args = self._next_args, None
+        kwargs, self._next_kwargs = self._next_kwargs, None
+        result = self._func(*args, **kwargs)
+        return result
