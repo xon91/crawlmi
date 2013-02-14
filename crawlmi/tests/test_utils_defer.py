0f6cd07e388bf86efcae77f4049f67e3f51e67ce
Michal Danilak <mimino.coder@gmail.com>
2013-02-14 23:28:21 +0100

    Created LoopingCall.

--- crawlmi/tests/test_utils_defer.py
+++ crawlmi/tests/test_utils_defer.py
@@ -1,7 +1,7 @@
 import unittest2
 
 from crawlmi.utils.clock import Clock
-from crawlmi.utils.defer import ScheduledCall
+from crawlmi.utils.defer import ScheduledCall, LoopingCall
 
 
 class ModifiedObject(object):
@@ -42,7 +42,7 @@ class ScheduledCallTest(unittest2.TestCase):
         # test initializing ScheduledCall without overriding its clock
         sc = ScheduledCall(self.obj.func, *self.default_args,
                            **self.default_kwargs)
-        sc.schedule(0)
+        sc.schedule()
         sc.cancel()
 
     def test_get_time_and_is_scheduled(self):
@@ -96,11 +96,11 @@ class ScheduledCallTest(unittest2.TestCase):
         self.clock.advance(5)
         self._check(over_args, {})
 
-    def test_nested_call(self):
+    def test_nested_schedule(self):
         def func(*args, **kwargs):
             self.obj.func(*args, **kwargs)
             self.sc.schedule()
-        self.sc._func = func
+        self.sc.func = func
         self.sc.schedule()
         self.assertEqual(self.obj.num_calls, 0)
         self.clock.advance(0)
@@ -108,7 +108,7 @@ class ScheduledCallTest(unittest2.TestCase):
         self.clock.advance(0)
         self.assertEqual(self.obj.num_calls, 2)
 
-    def test_nested_call_delay(self):
+    def test_nested_schedule_delay(self):
         args1 = ('a',)
         kwargs1 = {'a': 'b'}
         args2 = ('b',)
@@ -117,7 +117,7 @@ class ScheduledCallTest(unittest2.TestCase):
         def func(*args, **kwargs):
             self.obj.func(*args, **kwargs)
             self.sc.schedule(4, *args2, **kwargs2)
-        self.sc._func = func
+        self.sc.func = func
         self.sc.schedule(3, *args1, **kwargs1)
         self.clock.advance(3)
         self.assertIsNotNone(self.sc._call)
@@ -126,3 +126,108 @@ class ScheduledCallTest(unittest2.TestCase):
         self._check(args1, kwargs1)
         self.clock.advance(1)
         self._check(args2, kwargs2)
+
+
+class LoopingCallTest(unittest2.TestCase):
+    default_args = (10, 'hello')
+    default_kwargs = {'a': 47, 'b': 'c'}
+
+    def setUp(self):
+        self.clock = Clock()
+        self.obj = ModifiedObject()
+        self.sc = LoopingCall(self.obj.func, clock=self.clock,
+                              *self.default_args,
+                              **self.default_kwargs)
+
+    def _check(self, args, kwargs):
+        if args is None:
+            self.assertIsNone(self.obj.args)
+        else:
+            self.assertTupleEqual(self.obj.args, args)
+
+        if kwargs is None:
+            self.assertIsNone(self.obj.kwargs)
+        else:
+            self.assertEqual(self.obj.kwargs, kwargs)
+
+    def test_init(self):
+        # test initializing LoopingCall without overriding its clock
+        sc = LoopingCall(self.obj.func, *self.default_args,
+                           **self.default_kwargs)
+        sc.schedule()
+        sc.cancel()
+
+    def test_basic(self):
+        # scheduling
+        self.assertFalse(self.sc.is_scheduled())
+        self.sc.schedule(2, count=2, now=False)
+        # before the first call
+        self.assertTrue(self.sc.is_scheduled())
+        self.assertEqual(self.sc.get_time(), 2)
+        self.assertEqual(self.sc.calls_left(), 2)
+        self.clock.advance(1)
+        self.assertEqual(self.obj.num_calls, 0)
+        # after the first call
+        self.clock.advance(1)
+        self.assertEqual(self.obj.num_calls, 1)
+        self.assertEqual(self.sc.calls_left(), 1)
+        self._check(self.default_args, self.default_kwargs)
+        # after the second call
+        self.clock.advance(2)
+        self.assertEqual(self.obj.num_calls, 2)
+        self.assertEqual(self.sc.calls_left(), 0)
+        self.assertFalse(self.sc.is_scheduled())
+        # no more calls
+        self.clock.advance(20)
+        self.assertEqual(self.obj.num_calls, 2)
+
+    def test_now(self):
+        self.sc.schedule(2, count=2, now=True)
+        self.assertEqual(self.obj.num_calls, 0)
+        self.clock.advance(0)
+        self.assertEqual(self.obj.num_calls, 1)
+        self.clock.advance(20)
+        self.assertEqual(self.obj.num_calls, 2)
+
+    def test_infinite(self):
+        self.sc.schedule(2)
+        self.clock.pump([2] * 100)
+        self.assertEqual(self.obj.num_calls, 100)
+        self.assertTrue(self.sc.is_scheduled())
+        self.assertIsNone(self.sc.calls_left())
+
+    def test_cancel(self):
+        self.sc.schedule(2)
+        self.clock.advance(1)
+        self.sc.cancel()
+        self.clock.advance(20)
+        self.assertEqual(self.obj.num_calls, 0)
+
+    def test_reschedule(self):
+        self.sc.schedule(2)
+        self.clock.advance(1)
+        self.sc.schedule(5)
+        self.clock.advance(4)
+        self.assertEqual(self.obj.num_calls, 0)
+        self.clock.advance(1)
+        self.assertEqual(self.obj.num_calls, 1)
+
+    def test_no_delay(self):
+        self.sc.schedule()
+        self.assertEqual(self.obj.num_calls, 0)
+        self.clock.advance(0)
+        self.assertEqual(self.obj.num_calls, 1)
+        self.clock.advance(0)
+        self.assertEqual(self.obj.num_calls, 2)
+
+    def test_nested_schedule(self):
+        def func(*args, **kwargs):
+            self.obj.func(*args, **kwargs)
+            self.sc.schedule()
+        self.sc.func = func
+        self.sc.schedule()
+        self.assertEqual(self.obj.num_calls, 0)
+        self.clock.advance(0)
+        self.assertEqual(self.obj.num_calls, 1)
+        self.clock.advance(0)
+        self.assertEqual(self.obj.num_calls, 2)
--- crawlmi/utils/defer.py
+++ crawlmi/utils/defer.py
@@ -1,6 +1,66 @@
 from twisted.internet import reactor
 
 
+class LoopingCall(object):
+    '''Schedule the periodic call of the function.
+    '''
+
+    def __init__(self, func, *args, **kwargs):
+        # clock override is used in unittests
+        self.clock = kwargs.pop('clock', None) or reactor
+
+        self.func = func
+        self.args = args
+        self.kwargs = kwargs
+        self.delay = 0
+        self._call = None
+        self._remain_calls = None
+
+    def schedule(self, delay=0, count=None, now=False):
+        #assert countcount > 0, 'Number of calls for LoopingCall has to be positive!'
+        self.cancel()
+
+        self.delay = delay
+        self._remain_calls = count
+        if now:
+            self._call = self.clock.callLater(0, self)
+        else:
+            self._call = self.clock.callLater(self.delay, self)
+
+    def cancel(self):
+        if self._call:
+            self._call.cancel()
+            self._call = None
+
+    def calls_left(self):
+        '''Return the number of calls left.
+        Note: in a case of infinite loop, returns None.
+        '''
+        return self._remain_calls
+
+    def is_scheduled(self):
+        return self._call is not None
+
+    def get_time(self):
+        '''Return the time for which is this function scheduled.'''
+        if self._call:
+            return self._call.getTime()
+        else:
+            return 0
+
+    def __call__(self):
+        self._call = None
+        if self._remain_calls is not None:
+            self._remain_calls -= 1
+            if self._remain_calls <= 0:
+                self.cancel()
+            else:
+                self._call = self.clock.callLater(self.delay, self)
+        else:
+            self._call = self.clock.callLater(self.delay, self)
+        self.func(*self.args, **self.kwargs)
+
+
 class ScheduledCall(object):
     '''Schedule the function call for the future.
     Also ensure, that the function can be scheduled only once at a time.
@@ -11,14 +71,15 @@ class ScheduledCall(object):
         `default_kwargs`, when no arguments are given to `schedule` function.
         '''
 
-        self._func = func
-        self._default_args = default_args
-        self._default_kwargs = default_kwargs
+        # clock override is used in unittests
+        self.clock = default_kwargs.pop('clock', None) or reactor
+
+        self.func = func
+        self.default_args = default_args
+        self.default_kwargs = default_kwargs
         self._next_args = None
         self._next_kwargs = None
         self._call = None
-        # clock override is used in unittests
-        self.clock = default_kwargs.pop('clock', None) or reactor
 
     def schedule(self, delay=0, *args, **kwargs):
         '''Schedule the function call.
@@ -32,8 +93,8 @@ class ScheduledCall(object):
                 self._next_args = args
                 self._next_kwargs = kwargs
             else:
-                self._next_args = self._default_args
-                self._next_kwargs = self._default_kwargs
+                self._next_args = self.default_args
+                self._next_kwargs = self.default_kwargs
 
             self._call = self.clock.callLater(delay, self)
             return True
@@ -58,9 +119,8 @@ class ScheduledCall(object):
 
     def __call__(self):
         # order of the following operations is very important in a case
-        # when self._func schedules itself
+        # when self.func schedules itself
         self._call = None
         args, self._next_args = self._next_args, None
         kwargs, self._next_kwargs = self._next_kwargs, None
-        result = self._func(*args, **kwargs)
-        return result
+        self.func(*args, **kwargs)
