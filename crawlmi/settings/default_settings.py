9c17eff21e0c5334f0591501ea0188c6ad26d25c
Michal Danilak <mimino.coder@gmail.com>
2013-02-15 04:51:41 +0100

    Added more settings to `default_settings`.

--- crawlmi/settings/default_settings.py
+++ crawlmi/settings/default_settings.py
@@ -1,3 +1,18 @@
+# General settings
+
+STATS_CLASS = 'crawlmi.stats.MemoryStats'
+STATS_DUMP = True
+
+
+# Log settings
+
+LOG_ENABLED = True
+LOG_ENCODING = 'utf-8'
+LOG_FILE = None
+LOG_LEVEL = 'DEBUG'
+LOG_STDOUT = False
+
+
 # Downloader settings
 
 DOWNLOAD_HANDLERS = {
@@ -11,3 +26,15 @@ CONCURRENT_REQUESTS_PER_DOMAIN = 8  # use 0 not to limit requests per domain
 
 DOWNLOAD_DELAY = 0
 RANDOMIZE_DOWNLOAD_DELAY = True
+
+
+# Extensions
+
+EXTENSIONS_BASE = {}
+EXTENSIONS = {}
+
+
+# Downloader pipeline
+
+PIPELINE_BASE = {}
+PIPELINE = {}
