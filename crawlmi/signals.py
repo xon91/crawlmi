fcb4b0ccf47ceb7955612c5a442788e38a3e202c
Michal Danilak <mimino.coder@gmail.com>
2013-02-20 15:39:17 +0100

    Added `response_downloaded` signal. Added comments for signals.

--- crawlmi/signals.py
+++ crawlmi/signals.py
@@ -1,7 +1,14 @@
+# args:
 engine_started = object()
+# args:
 engine_stopped = object()
 
-# called when the request is pushed into the inq
+# invoked when the request successfully passes through downloader pipeline.
+# args: request
 request_received = object()
-# called when the response is popped out of the outq
+# invoked when the response is popped out of the outq
+# args: response
+response_downloaded = object()
+# invoked when the response successfully passes through downloader pipeline
+# args: response
 response_received = object()
