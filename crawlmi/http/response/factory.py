0d4f9bbe5992eed6f71a6cc195db22d4f0a20ff7
Michal Danilak <mimino.coder@gmail.com>
2013-03-09 06:56:19 +0100

    Added response factory methods.

---
+++ crawlmi/http/response/factory.py
@@ -0,0 +1,111 @@
+from mimetypes import MimeTypes
+from os import path
+
+from crawlmi.http.response import (Response, TextResponse, XmlResponse,
+                                   HtmlResponse)
+from crawlmi.utils.python import is_binary
+
+
+_mime_types = MimeTypes()
+_mt_filename = path.join(path.dirname(path.abspath(__file__)), 'mime.types')
+with open(_mt_filename, 'rb') as f:
+    _mime_types.readfp(f)
+
+_response_classes = {
+    'text/html': HtmlResponse,
+    'application/atom+xml': XmlResponse,
+    'application/rdf+xml': XmlResponse,
+    'application/rss+xml': XmlResponse,
+    'application/xhtml+xml': HtmlResponse,
+    'application/vnd.wap.xhtml+xml': HtmlResponse,
+    'application/xml': XmlResponse,
+    'application/json': TextResponse,
+    'application/javascript': TextResponse,
+    'application/x-javascript': TextResponse,
+    'text/xml': XmlResponse,
+    'text/*': TextResponse,
+}
+
+
+def from_mime_type(mime_type):
+    '''Return the most appropiate Response class for the given mime type.'''
+    if mime_type is None:
+        return Response
+    elif mime_type in _response_classes:
+        return _response_classes[mime_type]
+    else:
+        basetype = '%s/*' % mime_type.split('/')[0]
+        return _response_classes.get(basetype, Response)
+
+
+def from_filename(filename):
+    '''Return the most appropiate Response class from a filename.'''
+    mime_type, encoding = _mime_types.guess_type(filename)
+    if mime_type and not encoding:
+        return from_mime_type(mime_type)
+    else:
+        return Response
+
+
+def from_body(body):
+    '''Try to guess the appropiate response based on the body content.
+    This method is a bit magic and could be improved in the future, but
+    it's not meant to be used except for special cases where response types
+    cannot be guess using more straightforward methods.
+    '''
+    chunk = body[:5000]
+    if is_binary(chunk):
+        return from_mime_type('application/octet-stream')
+    elif '<html>' in chunk.lower():
+        return from_mime_type('text/html')
+    elif '<?xml' in chunk.lower():
+        return from_mime_type('text/xml')
+    else:
+        return from_mime_type('text')
+
+
+def from_content_type(content_type, content_encoding=None):
+    '''Return the most appropiate Response class from an HTTP Content-Type
+    header.
+    '''
+    if content_encoding:
+        return Response
+    mime_type = content_type.split(';')[0].strip().lower()
+    return from_mime_type(mime_type)
+
+
+def from_content_disposition(content_disposition):
+    try:
+        filename = content_disposition.split(';')[1].split('=')[1]
+        filename = filename.strip('"\'')
+        return from_filename(filename)
+    except IndexError:
+        return Response
+
+
+def from_headers(headers):
+    '''Return the most appropiate Response class by looking at the HTTP
+    headers.
+    '''
+    cls = Response
+    if 'Content-Type' in headers:
+        cls = from_content_type(headers['Content-type'],
+                                headers.get('Content-Encoding'))
+    if cls is Response and 'Content-Disposition' in headers:
+        cls = from_content_disposition(headers['Content-Disposition'])
+    return cls
+
+
+def from_args(headers=None, url=None, filename=None, body=None):
+    '''Guess the most appropiate Response class based on the given arguments.
+    '''
+    cls = Response
+    if headers is not None:
+        cls = from_headers(headers)
+    if cls is Response and url is not None:
+        cls = from_filename(url)
+    if cls is Response and filename is not None:
+        cls = from_filename(filename)
+    if cls is Response and body is not None:
+        cls = from_body(body)
+    return cls
---
+++ crawlmi/tests/test_response_factory.py
@@ -0,0 +1,83 @@
+from twisted.trial import unittest
+
+from crawlmi.http.headers import Headers
+from crawlmi.http.response import (Response, TextResponse, XmlResponse,
+                                   HtmlResponse)
+from crawlmi.http.response.factory import (from_mime_type, from_filename,
+        from_content_disposition, from_content_type, from_body, from_headers,
+        from_args, _mime_types)
+
+
+class FactoryTest(unittest.TestCase):
+    def test_from_filename(self):
+        mappings = [
+            ('data.bin', Response),
+            ('file.txt', TextResponse),
+            ('file.xml.gz', Response),
+            ('file.xml', XmlResponse),
+            ('file.html', HtmlResponse),
+            ('file.unknownext', Response),
+        ]
+        for source, cls in mappings:
+            retcls = from_filename(source)
+            self.assertIs(retcls, cls, 'Expected: %s  Received: %s' % (cls.__name__, retcls.__name__))
+
+    def test_from_content_disposition(self):
+        mappings = [
+            ('attachment; filename="data.xml"', XmlResponse),
+            ('attachment; filename=data.xml', XmlResponse),
+        ]
+        for source, cls in mappings:
+            retcls = from_content_disposition(source)
+            self.assertIs(retcls, cls, 'Expected: %s  Received: %s' % (cls.__name__, retcls.__name__))
+
+    def test_from_content_type(self):
+        mappings = [
+            ('text/html; charset=UTF-8', HtmlResponse),
+            ('text/xml; charset=UTF-8', XmlResponse),
+            ('application/xhtml+xml; charset=UTF-8', HtmlResponse),
+            ('application/vnd.wap.xhtml+xml; charset=utf-8', HtmlResponse),
+            ('application/xml; charset=UTF-8', XmlResponse),
+            ('application/octet-stream', Response),
+        ]
+        for source, cls in mappings:
+            retcls = from_content_type(source)
+            self.assertIs(retcls, cls, 'Expected: %s  Received: %s' % (cls.__name__, retcls.__name__))
+
+    def test_from_body(self):
+        mappings = [
+            ('\x03\x02\xdf\xdd\x23', Response),
+            ('Some plain text\ndata with tabs\t and null bytes\0', TextResponse),
+            ('<html><head><title>Hello</title></head>', HtmlResponse),
+            ('<?xml version="1.0" encoding="utf-8"', XmlResponse),
+        ]
+        for source, cls in mappings:
+            retcls = from_body(source)
+            self.assertIs(retcls, cls, 'Expected: %s  Received: %s' % (cls.__name__, retcls.__name__))
+
+    def test_from_headers(self):
+        mappings = [
+            ({'Content-Type': ['text/html; charset=utf-8']}, HtmlResponse),
+            ({'Content-Type': ['application/octet-stream'], 'Content-Disposition': ['attachment; filename=data.txt']}, TextResponse),
+            ({'Content-Type': ['text/html; charset=utf-8'], 'Content-Encoding': ['gzip']}, Response),
+        ]
+        for source, cls in mappings:
+            source = Headers(source)
+            retcls = from_headers(source)
+            self.assertIs(retcls, cls, 'Expected: %s  Received: %s' % (cls.__name__, retcls.__name__))
+
+    def test_from_args(self):
+        mappings = [
+            ({'url': 'http://www.example.com/data.csv'}, TextResponse),
+            # headers takes precedence over url
+            ({'headers': Headers({'Content-Type': ['text/html; charset=utf-8']}), 'url': 'http://www.example.com/item/'}, HtmlResponse),
+            ({'headers': Headers({'Content-Disposition': ['attachment; filename="data.xml.gz"']}), 'url': 'http://www.example.com/page/'}, Response),
+
+
+        ]
+        for source, cls in mappings:
+            retcls = from_args(**source)
+            self.assertIs(retcls, cls, 'Expected: %s  Received: %s' % (cls.__name__, retcls.__name__))
+
+    def test_custom_mime_types_loaded(self):
+        self.assertEqual(_mime_types.guess_type('x.crawlmitest')[0], 'x-crawlmi/test')
