76748cfb64b5c00821a498b534b7a28bca973cdd
Michal Danilak <mimino.coder@gmail.com>
2013-03-12 01:29:01 +0100

    Added selector attribute to HtmlResponse and XmlResponse.

--- crawlmi/http/response/html_response.py
+++ crawlmi/http/response/html_response.py
@@ -1,5 +1,14 @@
 from crawlmi.http.response import TextResponse
+from crawlmi.parser.selectors import HtmlXPathSelector
 
 
 class HtmlResponse(TextResponse):
-    pass
+    def __init__(self, *args, **kwargs):
+        super(HtmlResponse, self).__init__(*args, **kwargs)
+        self._selector = None
+
+    @property
+    def selector(self):
+        if self._selector is None:
+            self._selector = HtmlXPathSelector(self)
+        return self._selector
--- crawlmi/http/response/xml_response.py
+++ crawlmi/http/response/xml_response.py
@@ -1,5 +1,14 @@
 from crawlmi.http.response import TextResponse
+from crawlmi.parser.selectors import XmlXPathSelector
 
 
 class XmlResponse(TextResponse):
-    pass
+    def __init__(self, *args, **kwargs):
+        super(XmlResponse, self).__init__(*args, **kwargs)
+        self._selector = None
+
+    @property
+    def selector(self):
+        if self._selector is None:
+            self._selector = XmlXPathSelector(self)
+        return self._selector
