f90db953c2d919d9f275aa2cd57e7e37afdfc558
Michal Danilak <mimino.coder@gmail.com>
2013-03-13 05:54:19 +0100

    Added base_url property to HtmlResponse.

--- crawlmi/http/response/html_response.py
+++ crawlmi/http/response/html_response.py
@@ -1,14 +1,34 @@
+import re
+from urlparse import urljoin
+
 from crawlmi.http.response import TextResponse
 from crawlmi.parser.selectors import HtmlXPathSelector
+from crawlmi.utils.url import requote_url
+
+
+_base_url_re = re.compile(ur'<base\s+href\s*=\s*[\"\']\s*([^\"\'\s]+)\s*[\"\']', re.I)
 
 
 class HtmlResponse(TextResponse):
     def __init__(self, *args, **kwargs):
         super(HtmlResponse, self).__init__(*args, **kwargs)
         self._selector = None
+        self._base_url = None
 
     @property
     def selector(self):
         if self._selector is None:
             self._selector = HtmlXPathSelector(self)
         return self._selector
+
+    @property
+    def base_url(self):
+        if self._base_url is None:
+            self._base_url = self.url
+            chunk = self.text[:4096]
+            m = _base_url_re.search(chunk)
+            if m:
+                self._base_url = urljoin(self._base_url,
+                                         m.group(1).encode(self.encoding))
+            self._base_url = requote_url(self._base_url)
+        return self._base_url
---
+++ crawlmi/tests/test_http_html_response.py
@@ -0,0 +1,17 @@
+from twisted.trial import unittest
+
+from crawlmi.http.response.html_response import HtmlResponse
+from crawlmi.parser.selectors import HtmlXPathSelector
+
+
+class HtmlResponseTest(unittest.TestCase):
+    def setUp(self):
+        self.resp = HtmlResponse('http://github.com/', body='''<head>
+        <base href="http://www.w3schools.com/" target="_blank"></head>
+        <body></body>''')
+
+    def test_selector(self):
+        self.assertIsInstance(self.resp.selector, HtmlXPathSelector)
+
+    def test_base_url(self):
+        self.assertEqual(self.resp.base_url, 'http://www.w3schools.com/')
