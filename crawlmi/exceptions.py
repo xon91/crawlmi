36fdfe7e6e36bb5c60c239de9874f7cda18f8178
Michal Danilak <mimino.coder@gmail.com>
2013-04-04 22:11:21 +0200

    Added UsageError - raised for command-line usage errors.

--- crawlmi/exceptions.py
+++ crawlmi/exceptions.py
@@ -11,6 +11,14 @@ class NotSupported(Exception):
     pass
 
 
+# Commands
+class UsageError(Exception):
+    '''To indicate a command-line usage error.'''
+    def __init__(self, *a, **kw):
+        self.print_help = kw.pop('print_help', True)
+        super(UsageError, self).__init__(*a, **kw)
+
+
 # Downloader pipeline
 
 class RestartPipeline(Exception):
