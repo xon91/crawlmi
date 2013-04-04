82ce87fbf6aac4af27e7fcc70200fe639dd015cc
Michal Danilak <mimino.coder@gmail.com>
2013-04-05 00:38:06 +0200

    Big refactor of BaseCommand. It is now ready to be used.

--- crawlmi/commands/base.py
+++ crawlmi/commands/base.py
@@ -1,58 +1,103 @@
-from optparse import make_option
+from optparse import OptionGroup
 
 from crawlmi import log
-from crawlmi.spiders import BaseSpider
+from crawlmi.core.process import Process
+from crawlmi.exceptions import UsageError
 from crawlmi.settings import Settings
+from crawlmi.spiders import BaseSpider
 
 
 class BaseCommand(object):
-    '''Base class for writing crawlmi commands (named commands executed through
-    command line `crawlmi command ...`.
+    '''Base class for writing crawlmi commands - named commands executed
+    through command line `crawlmi command ...`.
 
-    Several attributes affect behavior at various steps along the way:
+    Attributes:
 
-    ``args``
-        A string listing the arguments accepted by the command,
-        suitable for use in help messages; e.g., a command which takes
-        a list of application names might set this to '<appname
-        appname ...>'.
-
-    ``requires_project``
+    `requires_project`
         A boolean indicating whether the command needs to be able to
-        import crawlmi settings; if ``True``, ``execute()`` will verify
+        import crawlmi settings; if `True`, `execute()` will verify
         that this is possible before proceeding. Default value is
-        ``False``.
+        `False`.
 
-    ``help``
-        A short description of the command, which will be printed in
-        help messages.
+    `default_settings`
+        Command specific settings to be applied and override the project
+        settings. By default, these settings are overriden through coommand
+        line `-s` option.
 
-    ``option_list``
-        This is the list of ``optparse`` options which will be fed
-        into the command's ``OptionParser`` for parsing arguments.
+    Process of initialization is:
+        1. `get_settings()` is called to receive the command specific settings.
+        2. `set_engine()` is called. Spider is still uninitialized.
+        3. `get_spider()` is called.
+        4. `handle()` is called. Engine is fully initialized. To start
+           crawlnig process, call `self.process.start()`.
     '''
 
-    option_list = (
-        make_option('--logfile', metavar='FILE', help='log file. if omitted stderr will be used'),
-        make_option('-L', '--loglevel', metavar='LEVEL', default=log.INFO, help='log level (default: INFO)'),
-        make_option('--nolog', action='store_true', help='disable logging completely'),
-        make_option('-s', '--set', action='append', default=[], metavar='NAME=VALUE', help='set/override setting (may be repeated)'),
-    )
-    args = ''
-    help = ''
-
     requires_project = False
+    default_settings = {}
+
+    def syntax(self):
+        '''Command syntax. Do not include command name. Don't use newlines.
+        '''
+        return ''
+
+    def short_desc(self):
+        '''A short description of the command. Don't use newlines.
+        '''
+        return ''
+
+    def help(self):
+        '''An extensive help for the command. It will be shown when using the
+        `help` command. It can contain newlines, since not post-formatting will
+        be applied to its contents.
+        '''
+        return self.short_desc()
+
+    def add_options(self, parser):
+        '''Populate option parse with options available for this command.
+        '''
+        group = OptionGroup(parser, "Global Options")
+        group.add_option('--logfile', metavar='FILE', help='log file. if omitted stderr will be used')
+        group.add_option('-L', '--loglevel', metavar='LEVEL', default=log.DEBUG, help='log level (default: DEBUG)')
+        group.add_option('--nolog', action='store_true', help='disable logging completely')
+        group.add_option('-s', '--set', action='append', default=[], metavar='NAME=VALUE', help='set/override setting (may be repeated)')
+        parser.add_option_group(group)
+
+    def get_settings(self, args, options):
+        '''Return command specific settings. Default behavior is to combine
+        `default_settings` with the settings received from command line.
+
+        `self.engine` is still not initialized, so don't use it.
+        '''
+        custom_settings = self.default_settings.copy()
+
+        try:
+            cmdline_settings = dict(x.split('=', 1) for x in options.set)
+            custom_settings.update(cmdline_settings)
+        except ValueError:
+            raise UsageError('Invalid -s value, use -s NAME=VALUE',
+                             print_help=False)
+
+        # logging behavior
+        if options.logfile:
+            custom_settings['LOG_ENABLED'] = True
+            custom_settings['LOG_FILE'] = options.logfile
+        if options.loglevel:
+            custom_settings['LOG_ENABLED'] = True
+            custom_settings['LOG_LEVEL'] = options.loglevel
+        if options.nolog:
+            custom_settings['LOG_ENABLED'] = False
+
+        return Settings(custom_settings)
 
     def set_engine(self, engine):
         self.engine = engine
+        self.settings = engine.settings
+        self.process = Process(engine)
 
-    def get_settings(self):
-        return Settings()
-
-    def get_spider(self):
+    def get_spider(self, args, options):
         return BaseSpider('')
 
-    def handle(self, *args, **options):
+    def handle(self, args, options):
         '''The actual logic of the command. Subclasses must implement this
         method.
         '''
