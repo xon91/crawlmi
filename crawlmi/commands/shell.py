f3720d2b9c440e7a453a4be89524c042bfc62e98
Michal Danilak <mimino.coder@gmail.com>
2013-04-06 01:40:39 +0200

    Added `shell` command.

---
+++ crawlmi/commands/shell.py
@@ -0,0 +1,41 @@
+from threading import Thread
+
+from crawlmi.commands.base import BaseCommand
+from crawlmi.exceptions import UsageError
+from crawlmi.shell import Shell
+
+
+class Command(BaseCommand):
+    requires_project = False
+    command_settings = {
+        'LOG_STATS_INTERVAL': 0,
+        'STATS_DUMP': False,
+    }
+
+    def syntax(self):
+        return '[url|file]'
+
+    def short_desc(self):
+        return 'Interactive console for given url.'
+
+    def add_options(self, parser):
+        BaseCommand.add_options(self, parser)
+
+    def update_vars(self, vars):
+        '''You can use this function to update the Crawlmi objects that will be
+        available in the shell.
+        '''
+
+    def handle(self, args, options):
+        if len(args) > 1:
+            raise UsageError()
+
+        url = args[0] if args else None
+        shell = Shell(self.engine, update_vars=self.update_vars)
+        self._start_shell_thread(shell, url)
+        self.process.start()
+
+    def _start_shell_thread(self, shell, url):
+        t = Thread(target=shell.start, kwargs={'url': url})
+        t.daemon = True
+        t.start()
