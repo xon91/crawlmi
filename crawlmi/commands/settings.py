b3bce76ab3e164fbcf570cfb2c3cee399bb7b04a
Michal Danilak <mimino.coder@gmail.com>
2013-04-07 00:26:26 +0200

    Added `settings` command.

---
+++ crawlmi/commands/settings.py
@@ -0,0 +1,38 @@
+from crawlmi.commands.base import BaseCommand
+
+
+class Command(BaseCommand):
+    requires_project = False
+    command_settings = {'LOG_ENABLED': False}
+
+    def syntax(self):
+        return '[options]'
+
+    def short_desc(self):
+        return 'Get settings values'
+
+    def add_options(self, parser):
+        BaseCommand.add_options(self, parser)
+        parser.add_option('--get', dest='get', metavar='SETTING',
+            help='print raw setting value')
+        parser.add_option('--getbool', dest='getbool', metavar='SETTING',
+            help='print setting value, intepreted as a boolean')
+        parser.add_option('--getint', dest='getint', metavar='SETTING',
+            help='print setting value, intepreted as an integer')
+        parser.add_option('--getfloat', dest='getfloat', metavar='SETTING',
+            help='print setting value, intepreted as an float')
+        parser.add_option('--getlist', dest='getlist', metavar='SETTING',
+            help='print setting value, intepreted as an float')
+
+    def handle(self, args, options):
+        settings = self.engine.settings
+        if options.get:
+            print settings.get(options.get)
+        elif options.getbool:
+            print settings.get_bool(options.getbool)
+        elif options.getint:
+            print settings.get_int(options.getint)
+        elif options.getfloat:
+            print settings.get_float(options.getfloat)
+        elif options.getlist:
+            print settings.get_list(options.getlist)
