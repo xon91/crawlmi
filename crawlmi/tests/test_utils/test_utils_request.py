1c2be7e452d13b345f9c45185c4cbc38485a8b40
Michal Danilak <mimino.coder@gmail.com>
2013-04-06 19:49:27 +0200

    Added `request_http_repr()` method.

---
+++ crawlmi/tests/test_utils/test_utils_request.py
@@ -0,0 +1,16 @@
+from twisted.trial import unittest
+
+from crawlmi.http.request import Request
+from crawlmi.utils.request import request_http_repr
+
+
+class UtilsRequestTest(unittest.TestCase):
+    def test_request_http_repr(self):
+        r1 = Request('http://www.example.com')
+        self.assertEqual(request_http_repr(r1), 'GET / HTTP/1.1\r\nHost: www.example.com\r\n\r\n')
+
+        r1 = Request('http://www.example.com/some/page.html?arg=1')
+        self.assertEqual(request_http_repr(r1), 'GET /some/page.html?arg=1 HTTP/1.1\r\nHost: www.example.com\r\n\r\n')
+
+        r1 = Request('http://www.example.com', method='POST', headers={'Content-type': 'text/html'}, body='Some body')
+        self.assertEqual(request_http_repr(r1), 'POST / HTTP/1.1\r\nHost: www.example.com\r\nContent-Type: text/html\r\n\r\nSome body')
--- crawlmi/utils/request.py
+++ crawlmi/utils/request.py
@@ -1,4 +1,23 @@
+from urlparse import urlunparse
+
 from twisted.internet.defer import Deferred
+
+
+def request_http_repr(request):
+    '''Return the raw HTTP representation (as string) of the given request.
+    This is provided only for reference since it's not the actual stream of
+    bytes that will be send when performing the request (that's controlled
+    by Twisted).
+    '''
+    parsed = request.parsed_url
+    path = urlunparse(('', '', parsed.path or '/', parsed.params, parsed.query, ''))
+    s = '%s %s HTTP/1.1\r\n' % (request.method, path)
+    s += 'Host: %s\r\n' % parsed.hostname
+    if request.headers:
+        s += request.headers.to_string() + '\r\n'
+    s += '\r\n'
+    s += request.body
+    return s
 
 
 def request_deferred(request):
