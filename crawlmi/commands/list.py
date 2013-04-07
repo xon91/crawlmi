5782740aa354c0b228b696c74fe183f5f3a419e1
Michal Danilak <mimino.coder@gmail.com>
2013-04-07 02:04:04 +0200

    Added `list` command.

---
+++ crawlmi/commands/list.py
@@ -0,0 +1,13 @@
+from crawlmi.commands.base import BaseCommand
+
+
+class Command(BaseCommand):
+    requires_project = True
+    command_settings = {'LOG_ENABLED': False}
+
+    def short_desc(self):
+        return 'List available spiders'
+
+    def run(self, args, options):
+        for sp_name in self.engine.spiders.get_spiders():
+            print sp_name
