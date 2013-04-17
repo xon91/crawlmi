dc64a06b8ec2af2ed2e06ebbca73e0ed9bd48633
Michal Danilak <mimino.coder@gmail.com>
2013-04-17 19:57:55 +0200

    `Filter` pipeline mw also keeps stats about filtered values.

--- crawlmi/middleware/pipelines/filter.py
+++ crawlmi/middleware/pipelines/filter.py
@@ -5,6 +5,7 @@ from crawlmi.http import TextResponse
 
 class Filter(object):
     def __init__(self, engine):
+        self.stats = engine.stats
         self.filter_nontext = engine.settings.get_bool('FILTER_NONTEXT_RESPONSE')
         self.url_limit = engine.settings.get_int('FILTER_URL_LENGTH_LIMIT')
         self.filter_non_200 = engine.settings.get_bool('FILTER_NON_200_RESPONSE_STATUS')
@@ -12,6 +13,7 @@ class Filter(object):
 
     def process_request(self, request):
         if self.url_limit and len(request.url) > self.url_limit:
+            self.stats.inc_value('filter/url_limit')
             log.msg(format='Filtering request (url length %(length)d): %(url)s',
                     level=log.DEBUG,
                     length=len(request.url),
@@ -21,6 +23,7 @@ class Filter(object):
 
     def process_response(self, response):
         if self.filter_non_200 and response.status != 200:
+            self.stats.inc_value('filter/non_200')
             log.msg(format='Filtering non-200 response (status %(status)d): %(url)s',
                     level=log.DEBUG,
                     status=response.status,
@@ -28,6 +31,7 @@ class Filter(object):
             return
 
         if self.filter_status(response.status):
+            self.stats.inc_value('filter/bad_status')
             log.msg(format='Filtering response with bad status (status %(status)d): %(url)s',
                     level=log.DEBUG,
                     status=response.status,
@@ -35,6 +39,7 @@ class Filter(object):
             return
 
         if self.filter_nontext and not isinstance(response, TextResponse):
+            self.stats.inc_value('filter/non_text')
             log.msg(format='Filtering nontext response: %(url)s',
                     level=log.DEBUG,
                     url=response.url)
--- crawlmi/tests/test_middleware/test_pipelines_filter.py
+++ crawlmi/tests/test_middleware/test_pipelines_filter.py
@@ -2,12 +2,19 @@ from twisted.trial import unittest
 
 from crawlmi.http import Request, Response, TextResponse
 from crawlmi.middleware.pipelines.filter import Filter
+from crawlmi.stats import MemoryStats
 from crawlmi.utils.test import get_engine
 
 
 class FilterUrlLengthTest(unittest.TestCase):
+    def _get_engine(self, **kwargs):
+        engine = get_engine(**kwargs)
+        engine.stats = MemoryStats(engine)
+        self.stats = engine.stats
+        return engine
+
     def test_nontext_response(self):
-        mw = Filter(get_engine(FILTER_NONTEXT_RESPONSE=True))
+        mw = Filter(self._get_engine(FILTER_NONTEXT_RESPONSE=True))
         req = Request('http://github.com/')
 
         good1 = TextResponse('', request=req)
@@ -18,12 +25,12 @@ class FilterUrlLengthTest(unittest.TestCase):
         bad2 = mw.process_response(bad1)
         self.assertIsNone(bad2)
 
-        mw = Filter(get_engine(FILTER_NONTEXT_RESPONSE=False))
+        mw = Filter(self._get_engine(FILTER_NONTEXT_RESPONSE=False))
         bad3 = mw.process_response(bad1)
         self.assertIs(bad1, bad3)
 
     def test_url_length_limit(self):
-        mw = Filter(get_engine(FILTER_URL_LENGTH_LIMIT=11))
+        mw = Filter(self._get_engine(FILTER_URL_LENGTH_LIMIT=11))
 
         good1 = Request('http://a.b/')
         good2 = mw.process_request(good1)
@@ -34,7 +41,7 @@ class FilterUrlLengthTest(unittest.TestCase):
         self.assertIsNone(bad2)
 
     def test_filter_non_200(self):
-        mw = Filter(get_engine(FILTER_NON_200_RESPONSE_STATUS=True))
+        mw = Filter(self._get_engine(FILTER_NON_200_RESPONSE_STATUS=True))
         req = Request('http://github.com/')
 
         good1 = Response('', request=req, status=200)
@@ -45,12 +52,12 @@ class FilterUrlLengthTest(unittest.TestCase):
         bad2 = mw.process_response(bad1)
         self.assertIsNone(bad2)
 
-        mw = Filter(get_engine(FILTER_NON_200_RESPONSE_STATUS=False))
+        mw = Filter(self._get_engine(FILTER_NON_200_RESPONSE_STATUS=False))
         bad3 = mw.process_response(bad1)
         self.assertIs(bad1, bad3)
 
     def test_response_status(self):
-        mw = Filter(get_engine(FILTER_RESPONSE_STATUS=lambda x: x != 201))
+        mw = Filter(self._get_engine(FILTER_RESPONSE_STATUS=lambda x: x != 201))
         req = Request('http://github.com/')
 
         good1 = Response('', request=req, status=201)
