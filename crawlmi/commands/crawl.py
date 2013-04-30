eb4f603dce96e01b3a11415022a5479672742ac9
Michal Danilak <mimino.coder@gmail.com>
2013-04-30 16:01:28 +0200

    Added `crawl` command.

---
+++ crawlmi/commands/crawl.py
@@ -0,0 +1,27 @@
+from crawlmi.commands.base import BaseCommand
+from crawlmi.exceptions import UsageError
+
+
+class Command(BaseCommand):
+    requires_project = True
+
+    def syntax(self):
+        return '[options] <spider>'
+
+    def short_desc(self):
+        return 'Start the crawling process of the spider'
+
+    def add_options(self, parser):
+        BaseCommand.add_options(self, parser)
+        parser.remove_option('--spider')  # spider is set through argument
+        parser.add_option('-a', dest='spargs', action='append', default=[],
+            metavar='NAME=VALUE', help='set spider argument (may be repeated)')
+
+    def get_spider(self, args, options):
+        if len(args) != 1:
+            raise UsageError()
+        return self.engine.spiders.create_spider_by_name(args[0])
+
+    def run(self, args, options):
+        self.engine.crawl_start_requests()
+        self.process.start()
