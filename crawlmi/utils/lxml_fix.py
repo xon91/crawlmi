13cbafe0b24f170c0db34a5cd6be00411a7a2c94
Michal Danilak <mimino.coder@gmail.com>
2013-05-21 21:16:27 +0200

    Module `crawlmi.utils.lxml_fix`, method `get_html()`:     - handle `str` objects     - restrict responses to `HtmlResponse`     - handle empty responses

--- crawlmi/utils/lxml_fix.py
+++ crawlmi/utils/lxml_fix.py
@@ -1,14 +1,27 @@
 import lxml.html
 
-from crawlmi.http import TextResponse
+from crawlmi.http import HtmlResponse
 
 
-def get_html(response_or_text):
-    if isinstance(response_or_text, TextResponse):
-        utf8_body = response_or_text.text.encode('utf-8')
-    elif isinstance(response_or_text, unicode):
-        utf8_body = response_or_text.encode('utf-8')
+def get_html(response_or_str, encoding='utf-8', base_url=None):
+    '''Bacause of lxml's annoying error:
+        "ValueError: Unicode strings with encoding declaration are not supported."
+    it can be a hassle to create etree correctly. This method solves it.
+    '''
+    if (not isinstance(response_or_str, HtmlResponse) and
+            not isinstance(response_or_str, basestring)):
+        raise TypeError('HtmlResponse or basestring expected.')
+
+    if isinstance(response_or_str, HtmlResponse):
+        body = response_or_str.text.encode('utf-8')
+        encoding = 'utf-8'
+        base_url = base_url or response_or_str.url
+    elif isinstance(response_or_str, unicode):
+        body = response_or_str.encode('utf-8')
+        encoding = 'utf-8'
     else:
-        raise TypeError('Expecting TextResponse or unicode object.')
-    parser = lxml.html.HTMLParser(recover=True, encoding='utf-8')
-    return lxml.html.fromstring(utf8_body, parser=parser)
+        body = response_or_str
+
+    body = body or '<html/>'  # empty body raises error in lxml
+    parser = lxml.html.HTMLParser(recover=True, encoding=encoding)
+    return lxml.html.fromstring(body, parser=parser, base_url=base_url)
