f3ccfbd6fc6198ddaf83226977db30c157af1cb0
Michal Danilak <mimino.coder@gmail.com>
2013-06-11 15:43:18 +0200

    Added `get_meta_refresh()` method.

--- crawlmi/tests/test_utils/test_utils_response.py
+++ crawlmi/tests/test_utils/test_utils_response.py
@@ -4,7 +4,8 @@ import urlparse
 from twisted.trial import unittest
 
 from crawlmi.http import Response, HtmlResponse
-from crawlmi.utils.response import open_in_browser, response_http_repr
+from crawlmi.utils.response import (open_in_browser, response_http_repr,
+                                    get_meta_refresh)
 
 
 class UtilsResponseTest(unittest.TestCase):
@@ -32,3 +33,66 @@ class UtilsResponseTest(unittest.TestCase):
         response = HtmlResponse(url, body=body)
         self.assertTrue(open_in_browser(response, _openfunc=browser_open), 'Browser not called')
         self.assertRaises(TypeError, open_in_browser, Response(url, body=body))
+
+    def test_get_meta_refresh(self):
+        url = 'http://example.org'
+        body = '''
+            <html>
+                <head><title>Dummy</title><meta http-equiv="refresh" content="5;url=http://example.org/newpage" /></head>
+                <body>blahablsdfsal&amp;</body>
+            </html>'''
+        self.assertEqual(get_meta_refresh(HtmlResponse(url, body=body)), (5, 'http://example.org/newpage'))
+
+        # refresh without url should return (None, None)
+        body = '''<meta http-equiv="refresh" content="5" />'''
+        self.assertEqual(get_meta_refresh(HtmlResponse(url, body=body)), (None, None))
+
+        body = '''<meta http-equiv="refresh" content="5;
+            url=http://example.org/newpage" /></head>'''
+        self.assertEqual(get_meta_refresh(HtmlResponse(url, body=body)), (5, 'http://example.org/newpage'))
+
+        # meta refresh in multiple lines
+        body = '''<html><head>
+<META
+HTTP-EQUIV="Refresh"
+CONTENT="1; URL=http://example.org/newpage">'''
+        self.assertEqual(get_meta_refresh(HtmlResponse(url, body=body)), (1, 'http://example.org/newpage'))
+
+        # entities in the redirect url
+        body = '''<meta http-equiv="refresh" content="3; url=&#39;http://www.example.com/other&#39;">'''
+        self.assertEqual(get_meta_refresh(HtmlResponse(url, body=body)), (3, 'http://www.example.com/other'))
+
+        url = 'http://example.com/page/this.html'
+        # relative redirects
+        body = '''<meta http-equiv="refresh" content="3; url=other.html">'''
+        self.assertEqual(get_meta_refresh(HtmlResponse(url, body=body)), (3, 'http://example.com/page/other.html'))
+
+        # non-standard encodings (utf-16)
+        url = 'http://example.com'
+        body = '''<meta http-equiv="refresh" content="3; url=http://example.com/redirect">'''
+        body = body.decode('ascii').encode('utf-16')
+        self.assertEqual(get_meta_refresh(HtmlResponse(url, body=body, encoding='utf-16')),
+            (3, 'http://example.com/%FF%FEh%00t%00t%00p%00:%00/%00/%00e%00x%00a%00m%00p%00l%00e%00.%00c%00o%00m%00/%00r%00e%00d%00i%00r%00e%00c%00t%00'))
+
+        # non-ascii chars in the url (utf8 - default)
+        body = '''<meta http-equiv="refresh" content="3; url=http://example.com/to\xc2\xa3">'''
+        self.assertEqual(get_meta_refresh(HtmlResponse(url, body=body)), (3, 'http://example.com/to%C2%A3'))
+
+        # non-ascii chars in the url (latin1)
+        body = '''<meta http-equiv="refresh" content="3; url=http://example.com/to\xa3">'''
+        self.assertEqual(get_meta_refresh(HtmlResponse(url, body=body, encoding='latin1')), (3, 'http://example.com/to%A3'))
+
+        # html commented meta refresh header must not directed
+        body = '''<!--<meta http-equiv="refresh" content="3; url=http://example.com/">-->'''
+        self.assertEqual(get_meta_refresh(HtmlResponse(url, body=body)), (None, None))
+
+        # html comments must not interfere with uncommented meta refresh header
+        body = '''<!-- commented --><meta http-equiv="refresh" content="3; url=http://example.com/">-->'''
+        self.assertEqual(get_meta_refresh(HtmlResponse(url, body=body)), (3, 'http://example.com/'))
+
+        # float refresh intervals
+        body = '''<meta http-equiv="refresh" content=".1;URL=index.html" />'''
+        self.assertEqual(get_meta_refresh(HtmlResponse(url, body=body)), (0.1, 'http://example.com/index.html'))
+
+        body = '''<meta http-equiv="refresh" content="3.1;URL=index.html" />'''
+        self.assertEqual(get_meta_refresh(HtmlResponse(url, body=body)), (3.1, 'http://example.com/index.html'))
--- crawlmi/utils/response.py
+++ crawlmi/utils/response.py
@@ -1,10 +1,17 @@
 import os
+import re
 import tempfile
+import urlparse
 import webbrowser
 
 from twisted.web.http import RESPONSES
 
 from crawlmi.http import HtmlResponse, TextResponse
+from crawlmi.utils.html import remove_entities
+from crawlmi.utils.python import to_str
+from crawlmi.utils.regex import (html_script_re, html_noscript_re,
+                                 html_comment_re)
+from crawlmi.utils.url import requote_url
 
 
 def response_http_repr(response):
@@ -39,3 +46,23 @@ def open_in_browser(response, _openfunc=webbrowser.open):
     os.write(fd, body)
     os.close(fd)
     return _openfunc('file://%s' % fname)
+
+
+_meta_refresh_re = re.compile(ur'<meta[^>]*http-equiv[^>]*refresh[^>]*content\s*=\s*(?P<quote>["\'])(?P<int>(\d*\.)?\d+)\s*;\s*url=(?P<url>.*?)(?P=quote)', re.DOTALL | re.IGNORECASE)
+
+def get_meta_refresh(response):
+    '''Parse the http-equiv refrsh parameter from the given HTML response.
+    Return tuple (interval, url).'''
+    text = remove_entities(response.text[0:4096])
+    text = html_comment_re.sub(u'', text)
+    text = html_noscript_re.sub(u'', text)
+    text = html_script_re.sub(u'', text)
+
+    m = _meta_refresh_re.search(text)
+    if m:
+        interval = float(m.group('int'))
+        url = requote_url(to_str(m.group('url').strip(' "\''), response.encoding))
+        url = urlparse.urljoin(response.url, url)
+        return (interval, url)
+    else:
+        return (None, None)
