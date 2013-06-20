7c9108cd62138cbe875c695ec9cd388a2d1946ce
Michal Danilak <mimino.coder@gmail.com>
2013-06-20 23:54:41 +0200

    Added tags and signal to clear the storage to `DuplicateFilter` mw.

--- crawlmi/middleware/pipelines/duplicate_filter.py
+++ crawlmi/middleware/pipelines/duplicate_filter.py
@@ -1,13 +1,29 @@
+from collections import defaultdict
+
+from crawlmi.signals import Signal
 from crawlmi.utils.request import request_fingerprint
+
+
+# send this signal to clear the filter's storage
+# args: [df_tag]
+clear_duplicate_filter = Signal('clear_duplicate_filter')
 
 
 class DuplicateFilter(object):
     def __init__(self, engine):
-        self.fingerprints = set()
+        self.fingerprints = defaultdict(set)
+        engine.signals.connect(self.clear_duplicate_filter,
+                               signal=clear_duplicate_filter)
 
     def process_request(self, request):
+        df_tag = request.meta.get('df_tag')
+        fps = self.fingerprints[df_tag]
         fp = request_fingerprint(request)
-        if fp in self.fingerprints:
+        if fp in fps:
             return
-        self.fingerprints.add(fp)
+        fps.add(fp)
         return request
+
+    def clear_duplicate_filter(self, df_tag=None):
+        if df_tag in self.fingerprints:
+            del self.fingerprints[df_tag]
--- crawlmi/tests/test_middleware/test_pipelines_duplicate_filter.py
+++ crawlmi/tests/test_middleware/test_pipelines_duplicate_filter.py
@@ -1,18 +1,40 @@
 from twisted.trial import unittest
 
 from crawlmi.http import Request
-from crawlmi.middleware.pipelines.duplicate_filter import DuplicateFilter
+from crawlmi.middleware.pipelines.duplicate_filter import DuplicateFilter, clear_duplicate_filter
 from crawlmi.utils.test import get_engine
 
 
 class DuplicateFilterTest(unittest.TestCase):
     def test_process_request(self):
-        mw = DuplicateFilter(get_engine())
+        engine = get_engine()
+        mw = DuplicateFilter(engine)
 
         r1 = Request('http://test.org/1')
         r2 = Request('http://test.org/2')
         r3 = Request('http://test.org/2')
 
         self.assertIs(mw.process_request(r1), r1)
+        self.assertIs(mw.process_request(r2), r2)
+        self.assertIsNone(mw.process_request(r3))
+
+        engine.signals.send(clear_duplicate_filter)
+        self.assertIs(mw.process_request(r3), r3)
+
+    def test_tags(self):
+        engine = get_engine()
+        mw = DuplicateFilter(engine)
+
+        r1 = Request('http://test.org/', meta={'df_tag': '1'})
+        r2 = Request('http://test.org/', meta={'df_tag': '2'})
+        r3 = Request('http://test.org/', meta={'df_tag': '2'})
+
+        self.assertIs(mw.process_request(r1), r1)
+        self.assertIs(mw.process_request(r2), r2)
+        self.assertIsNone(mw.process_request(r3))
+
+        engine.signals.send(clear_duplicate_filter, df_tag='2')
+
+        self.assertIsNone(mw.process_request(r1))
         self.assertIs(mw.process_request(r2), r2)
         self.assertIsNone(mw.process_request(r3))
