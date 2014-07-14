2452fda34b7f3136fb99209bc0507acb885053af
Michal Danilak <mimino.coder@gmail.com>
2014-07-14 14:49:24 +0200

    Added few monkeypathces to fix external libraries.

--- crawlmi/__init__.py
+++ crawlmi/__init__.py
@@ -1,2 +1,13 @@
 version_info = (0, 1, 0)
 __version__ = '0.1.0'
+
+
+# Ignore noisy twisted deprecation warnings
+import warnings
+warnings.filterwarnings('ignore', category=DeprecationWarning, module='twisted')
+del warnings
+
+
+# Apply monkey patches to fix issues in external libraries
+from . import _monkeypatches
+del _monkeypatches
