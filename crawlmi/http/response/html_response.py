c58cec6a5a60eab11681acfba8b0f81883d98a05
Michal Danilak <mimino.coder@gmail.com>
2016-01-20 00:22:44 +0100

    Added xextract to external requirements. Added `extractor` to Response classes.

--- crawlmi/http/response/html_response.py
+++ crawlmi/http/response/html_response.py
@@ -5,6 +5,8 @@ from crawlmi.http import TextResponse
 from crawlmi.parser.selectors import HtmlXPathSelector
 from crawlmi.utils.url import requote_url
 
+from xextract.extractors.lxml_extractor import HtmlXPathExtractor
+
 
 _base_url_re = re.compile(r'<base\s+href\s*=\s*[\"\']\s*([^\"\'\s]+)\s*[\"\']', re.IGNORECASE)
 
@@ -13,6 +15,7 @@ class HtmlResponse(TextResponse):
     def __init__(self, *args, **kwargs):
         super(HtmlResponse, self).__init__(*args, **kwargs)
         self._selector = None
+        self._extractor = None
         self._base_url = None
 
     @property
@@ -20,6 +23,12 @@ class HtmlResponse(TextResponse):
         if self._selector is None:
             self._selector = HtmlXPathSelector(self)
         return self._selector
+
+    @property
+    def extractor(self):
+        if self._extractor is None:
+            self._extractor = HtmlXPathExtractor(self.text)
+        return self._extractor
 
     @property
     def base_url(self):
--- crawlmi/http/response/xml_response.py
+++ crawlmi/http/response/xml_response.py
@@ -1,11 +1,13 @@
 from crawlmi.http import TextResponse
 from crawlmi.parser.selectors import XmlXPathSelector
+from xextract.extractors.lxml_extractor import XmlXPathExtractor
 
 
 class XmlResponse(TextResponse):
     def __init__(self, *args, **kwargs):
         super(XmlResponse, self).__init__(*args, **kwargs)
         self._selector = None
+        self._extractor = None
 
     @property
     def selector(self):
@@ -13,3 +15,9 @@ class XmlResponse(TextResponse):
             self._selector = XmlXPathSelector(self)
             self._selector.namespaces = self._selector._root.nsmap
         return self._selector
+
+    @property
+    def extractor(self):
+        if self._extractor is None:
+            self._extractor = XmlXPathExtractor(self.text)
+        return self._extractor
