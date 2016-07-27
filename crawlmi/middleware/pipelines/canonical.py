ac95d91f9f7e8363a9cf2da24c3e5e1ff04280b0
Michal Danilak <mimino.coder@gmail.com>
2016-07-27 11:18:53 +0200

    Fixed `canonical` middleware.

--- crawlmi/middleware/pipelines/canonical.py
+++ crawlmi/middleware/pipelines/canonical.py
@@ -2,6 +2,7 @@ import re
 import urlparse
 
 from crawlmi.http import HtmlResponse
+from crawlmi.utils.url import requote_url
 
 
 class Canonical(object):
@@ -10,12 +11,15 @@ class Canonical(object):
         self.canonical_tag_re = re.compile(r'<link\s+rel\s*=\s*[\"\']canonical[\"\']\s+href\s*=\s*[\"\']\s*([^\"\'\s]+)\s*[\"\']', re.IGNORECASE)
 
     def process_response(self, response):
+        canonical_url = None
         if 'Link' in response.headers:
             m = self.canonical_header_re.search(response.headers['link'])
             if m:
-                response.meta['canonical_url'] = urlparse.urljoin(response.base_url, m.group(1))
+                canonical_url = m.group(1)
         if isinstance(response, HtmlResponse):
             m = self.canonical_tag_re.search(response.body[:4096])
             if m:
-                response.meta['canonical_url'] = urlparse.urljoin(response.base_url, m.group(1))
+                canonical_url = m.group(1)
+        if canonical_url:
+            response.meta['canonical_url'] = requote_url(urlparse.urljoin(response.base_url, canonical_url))
         return response
